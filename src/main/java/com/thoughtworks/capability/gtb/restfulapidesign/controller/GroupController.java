dbb906fd9d8f2b045cfa53f7fff94c7f37013af0
crystalfn <969274162@qq.com>
2020-11-07 17:23:23 +0800

    [fangni] fix: fix group api in restful

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/GroupController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/controller/GroupController.java
@@ -5,6 +5,7 @@ import com.thoughtworks.capability.gtb.restfulapidesign.service.GroupService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class GroupController {
         this.groupService = groupService;
     }
 
-    @GetMapping("/group")
+    @PostMapping("/group")
     public List<Group> group() {
         return groupService.group();
     }
